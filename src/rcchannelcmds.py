a6fc37ac40531192ad8a559da5abf3881116b7e4
Jon Trowbridge <trow@ximian.com>
2002-06-07 19:27:17 +0000

    Print usage info if --help or -? is passed on the command line. Small
    2002-06-07  Jon Trowbridge  <trow@ximian.com>
    
    	* rc: Print usage info if --help or -? is passed on the command
    	line.  Small option-handling clean-ups.  Added license.
    
    	* rcformat.py (opt_table): Added.  Generates a formated description
    	of a list of command options.  Added license.
    
    	* rccommand.py: Changed command registration slightly.  Hacked
    	construct to accept unambiguous partial matches (i.e. "sub"
    	for "subscribe").  Added usage method to RCCommand.  Moved
    	default options into an RCCommand method.  Added license.
    
    	* rcchannelcmds.py: Added support for more options, improved
    	error handling.  Added license.

--- src/rcchannelcmds.py
+++ src/rcchannelcmds.py
@@ -1,5 +1,28 @@
+###
+### Copyright 2002 Ximian, Inc.
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation; either version 2 of the License, or
+### (at your option) any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program; if not, write to the Free Software
+### Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+###
+
+import sys
 import rcformat
 import rccommand
+
+###
+### Useful channel-related subroutines
+###
 
 cached_channel_list = []
 
@@ -8,6 +31,9 @@ def get_channels(server):
     if not cached_channel_list:
         cached_channel_list = server.rcd.packsys.get_channels()
     return cached_channel_list
+
+def channel_to_str(c):
+    return "'" + c["name"] + "' (ID# " + str(c["id"]) + ")"
 
 def get_channel_by_id(server, id):
     channels = get_channels(server)
@@ -21,7 +47,19 @@ def channel_id_to_name(server, id):
         if str(c["id"]) == str(id):
             return c["name"]
 
+
+###
+### Channel-related commands
+###
+
 class ListChannelsCmd(rccommand.RCCommand):
+
+    def name(self):
+        return "channels"
+
+    def local_opt_table(self):
+        return [["s", "subscribed", "", "Only list subscribed channels"],
+                ["u", "unsubscribed", "", "Only list unsubscribed channels"]]
 
     def execute(self, server, options_dict, non_option_args):
 
@@ -30,41 +68,84 @@ class ListChannelsCmd(rccommand.RCCommand):
 
         for c in channels:
 
+            show = 1
+
             if c["subscribed"]:
                 subflag = " Yes "
+                if options_dict.has_key("unsubscribed"):
+                    show = 0
             else:
                 subflag = ""
-                
-            channel_table.append([subflag, str(c["id"]), c["name"]])
+                if options_dict.has_key("subscribed"):
+                    show = 0
 
+            if show:
+                channel_table.append([subflag, str(c["id"]), c["name"]])
+
+        if channel_table:
             channel_table.sort(lambda x, y:cmp(x[2],y[2]))
-
-        rcformat.tabular(["subd?", "ID", "Name"], channel_table)
+            rcformat.tabular(["subd?", "ID", "Name"], channel_table)
+        else:
+            if options_dict.has_key("unsubscribed"):
+                print "--- No unsubscribed channels ---"
+            elif options_dict.has_key("subscribed"):
+                print "--- No subscribed channels ---"
+            else:
+                print "--- No channels available ---"
 
 
 class SubscribeCmd(rccommand.RCCommand):
 
+    def name(self):
+        return "subscribe"
+
+    def local_opt_table(self):
+        return [["s", "strict", "", "Fail if attempting to subscribe to an already-subscribed channel"]]
+
     def execute(self, server, options_dict, non_option_args):
 
+        failed = 0
         for a in non_option_args:
-            if not get_channel_by_id(server, a):
-                print "Invalid channel id: " + a
-                sys.exit(1)
+            c = get_channel_by_id(server, a)
+            if not c:
+                print "Invalid channel: '" + a + "'"
+                failed = 1
+            elif options_dict.has_key("strict") and c["subscribed"]:
+                print "Already subscribed to channel "+channel_to_str(c)
+                failed = 1
+
+        if failed:
+            sys.exit(1)
 
         for a in non_option_args:
             c = get_channel_by_id(server, a)
             if c and server.rcd.packsys.subscribe(int(a)):
-                print "Subscribed to channel '"+c["name"]+"' (ID# "+str(c["id"])+")"
+                print "Subscribed to channel '"+channel_to_str(c)
 
 
 class UnsubscribeCmd(rccommand.RCCommand):
 
+    def name(self):
+        return "unsubscribe"
+
+    def local_opt_table(self):
+        return [["s", "strict", "", "Fail if attempting to unsubscribe from a non-subscribed channel"]]
+
+
     def execute(self, server, options_dict, non_option_args):
 
+        failed = 0
         for a in non_option_args:
-            if not get_channel_by_id(server, a):
-                print "Invalid channel id: " + a
-                sys.exit(1)
+            c = get_channel_by_id(server, a)
+            if not c:
+                print "Invalid channel id: '" + a + "'"
+                failed = 1
+            elif options_dict.has_key("strict") and not c["subscribed"]:
+                print "Not subscribed to channel "+channel_to_str(c)
+                failed = 1
+
+        if failed:
+            sys.exit(1)
 
         for a in non_option_args:
             c = get_channel_by_id(server, a)
@@ -75,6 +156,6 @@ class UnsubscribeCmd(rccommand.RCCommand):
 
 
 
-rccommand.register("channels", "List available channels", ListChannelsCmd)
-rccommand.register("subscribe", "Subscribe to a channel", SubscribeCmd)
-rccommand.register("unsubscribe", "Unsubscribe from a channel", UnsubscribeCmd)
+rccommand.register(ListChannelsCmd, "List available channels")
+rccommand.register(SubscribeCmd, "Subscribe to a channel")
+rccommand.register(UnsubscribeCmd, "Unsubscribe from a channel")
--- src/rccommand.py
+++ src/rccommand.py
@@ -1,18 +1,57 @@
+###
+### Copyright 2002 Ximian, Inc.
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation; either version 2 of the License, or
+### (at your option) any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program; if not, write to the Free Software
+### Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+###
+
 import string
+import rcformat
 
 command_dict = {}
 
-def register(name, description, constructor):
+def register(constructor, description):
+    obj = constructor()
+    name = obj.name()
     if command_dict.has_key(name):
         print "Command name collision: '"+name+"'"
     else:
         command_dict[name] = (description, constructor)
 
+
 def construct(name):
-    if command_dict.has_key(name):
-        cons = (command_dict[name])[1]
-        return cons()
-    return None
+    valid_names = command_dict.keys()
+
+    ### Allow partial command names when it is unamiguous
+    ### (i.e. "sub" matches "subscribe")
+    matches = []
+    for n in valid_names:
+        if string.find(n, name) == 0:
+            matches.append(n)
+
+    if len(matches) == 0:
+        print "Unknown command."
+        return None
+
+    if len(matches) > 1:
+        print "Ambiguous command:"
+        print "'"+name+"' matches "+ string.join(matches, ", ")
+        return None
+        
+    cons = (command_dict[matches[0]])[1]
+    return cons()
+
 
 def usage():
     print "Usage: rc <command> <options> ..."
@@ -27,18 +66,48 @@ def usage():
     else:
         print "<< No commands found --- something is wrong! >>"
 
+
 class RCCommand:
 
-    def opt_table(self):
+    def name(self):
+        return "Unknown!"
+
+    def default_opt_table(self):
+        return [["U", "user",     "username", "Specify user name"],
+                ["P", "password", "password", "Specify password"],
+                ["h", "host",     "hostname", "Contact daemon on specified host"],
+                ["n", "dry-run",  "",         "Don't perform operation"],
+                ["",  "version",  "",         "Print client version and exit"],
+                ["v", "verbose",  "",         "Verbose output"],
+                ["t", "terse",    "",         "Terse output"],
+                ["",  "debug",    "",         "Debugging output"],
+                ["",  "batch",    "",         "Run in batch mode"],
+                ["?", "help",     "",         "Get help on a specific command"]]
+
+
+    def local_opt_table(self):
         return [];
+
+    def opt_table(self):
+        return self.default_opt_table() + self.local_opt_table()
+
+    def usage(self):
+        opts = self.default_opt_table()
+        if opts:
+            print "General Options:"
+            rcformat.opt_table(opts)
+            print
+
+        opts = self.local_opt_table()
+        if opts:
+            print "'" + self.name() + "' Options:"
+            rcformat.opt_table(opts)
+            print
+        
 
     def execute(self, server, options_dict, non_option_args):
         print "Execute not implemented!"
         sys.exit(1)
-
-
-register("foo", "dummy command", RCCommand)
-register("bar", "dummy command", RCCommand)
 
     
     
