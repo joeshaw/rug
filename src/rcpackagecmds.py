14fb79f7b0591e5a4f7f39260f0a583ce3d32702
Joe Shaw <joe@ximian.com>
2002-06-10 18:04:33 +0000

    Get the system packages from the daemon so we can show if each package is
    2002-06-10  Joe Shaw  <joe@ximian.com>
    
    	* rcpackagecmds.py (install_indicator): Get the system packages
    	from the daemon so we can show if each package is installed on the
    	system.

--- src/rcpackagecmds.py
+++ src/rcpackagecmds.py
@@ -20,11 +20,19 @@ import rcformat
 import rccommand
 import rcchannelcmds
 
+cached_system_packages = {}
+
 def get_packages(server, channel):
     return server.rcd.packsys.query([["channel","is",str(channel["id"])]])
 
-def install_indicator(package):
-    if (package["installed"]):
+def install_indicator(server, package):
+    global cached_system_packages
+    if not cached_system_packages:
+        system_packages = server.rcd.packsys.query([["installed","is","true"]])
+        for a in system_packages:
+            cached_system_packages[a["name"]] = a
+
+    if cached_system_packages.has_key(package["name"]):
         return "i"
     else:
         return ""
@@ -46,7 +54,8 @@ class PackageListCmd(rccommand.RCCommand):
             else:
                 packages = get_packages(server, c);
 
-                map(lambda x,pt=package_table:pt.append([install_indicator(x), str(x["channel"]), x["name"], x["version"] + "-" + x["release"]]), packages)
+                for p in packages:
+                    package_table.append([install_indicator(server, p), str(p["channel"]), p["name"], p["version"] + "-" + p["release"]])
 
         if package_table:
             package_table.sort(lambda x, y:cmp(x[2],y[2]))
