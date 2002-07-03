c7603a5ccffb38082d2a4022bd91ef2419c263d2
Joe Shaw <joe@ximian.com>
2002-07-03 20:46:40 +0000

    Add a command for setting preferences, and try to do some type mangling to
    2002-07-03  Joe Shaw  <joe@ximian.com>
    
    	* rcprefscmds.py: Add a command for setting preferences, and try
    	to do some type mangling to make it all work out.

--- src/rcprefscmds.py
+++ src/rcprefscmds.py
@@ -15,25 +15,75 @@
 ### Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 ###
 
+import string
+import sys
 import rctalk
 import rcformat
 import rccommand
 import rcmain
 import ximian_xmlrpclib
 
-class PrefsCmd(rccommand.RCCommand):
+class PrefsSetCmd(rccommand.RCCommand):
 
     def name(self):
-        return "preferences"
+        return "set-prefs"
 
     def aliases(self):
-        return ["prefs"]
+        return ["set"]
 
     def arguments(self):
-        return "some stuff..."
+        return "<pref name> <value>"
 
     def description_short(self):
-        return "List, get, and set system preferences"
+        return "Set a system preference"
+
+    def execute(self, server, options_dict, non_option_args):
+        if len(non_option_args) < 2:
+            self.usage()
+            sys.exit(0)
+
+        try:
+            pref = server.rcd.prefs.get_pref(non_option_args[0])
+        except ximian_xmlrpclib.Fault, f:
+            if f.faultCode == -630:
+                rctalk.error("There is no preference named '" + non_option_args[0] + "'")
+                sys.exit(1)
+            else:
+                raise
+
+        if string.lower(non_option_args[1]) == "true":
+            value = ximian_xmlrpclib.True
+        elif string.lower(non_option_args[1]) == "false":
+            value = ximian_xmlrpclib.False
+        else:
+            try:
+                value = int(non_option_args[1])
+            except ValueError:
+                value = non_option_args[1]
+
+        try:
+            server.rcd.prefs.set_pref(non_option_args[0], value)
+        except ximian_xmlrpclib.Fault, f:
+            # Catch type mismatches
+            if f.faultCode == -501:
+                # FIXME: This error message sucks
+                rctalk.error("Can't set preference: " + f.faultString)
+            else:
+                raise
+
+class PrefsListCmd(rccommand.RCCommand):
+
+    def name(self):
+        return "get-prefs"
+
+    def aliases(self):
+        return ["get", "prefs"]
+
+    def arguments(self):
+        return "<pref name> <pref name> ..."
+
+    def description_short(self):
+        return "List system preferences"
 
     def execute(self, server, options_dict, non_option_args):
         headers = ["Name", "Value"]
@@ -57,5 +107,6 @@ class PrefsCmd(rccommand.RCCommand):
         if pref_table:
             rcformat.tabular(headers, pref_table)
 
-rccommand.register(PrefsCmd)
+rccommand.register(PrefsListCmd)
+rccommand.register(PrefsSetCmd)
             
